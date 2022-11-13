f486e1498c45cdd35658305db9a0e85c39a5ebc5
Aramide Ramadan <92337603+mister-abdurahman@users.noreply.github.com>
2022-11-13 17:37:31 +0100

    i added test to the API

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -33,7 +33,7 @@ async function login(req, res, next){
         const payload = { user: {id: user._id, email: user.email} } 
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
         user.password = undefined //a trick to prevent the password from being displayed, does not affect the actual password.
-        return res.status(200).json({status: "success",token, user}) 
+        return res.status(200).json({status: "success", token, user}) 
         } catch (error) {
         console.log(error)
         res.status(500).send("Something broke")
